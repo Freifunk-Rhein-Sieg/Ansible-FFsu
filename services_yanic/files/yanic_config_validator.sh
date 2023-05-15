ef8b880ad028234edfe771bbf74e89e946a68f17
Claus Marxmeier <claus@marxmeier.de>
2023-05-15 20:35:50 +0200

    Update yanic_config_validator.sh

--- services_yanic/files/yanic_config_validator.sh
+++ services_yanic/files/yanic_config_validator.sh
@@ -2,7 +2,7 @@
 for bat in `ifconfig | grep bat | cut -d ' ' -f 1`; do
     echo "config file for $bat is: "
     ls /etc/yanic/yanic_$bat.conf
-    address=`ifconfig $bat | grep fe80 | awk  '{print $2}' | cut -d '/' -f 1`
+    address=`ifconfig $bat | grep fe80 -m 1 | awk  '{print $2}' | cut -d '/' -f 1`
     echo "configured ip_adress is: $address"
     echo "writing $address to config file..."
     sed -i 's/^\(ip_address\s*=\s*\).*$/\1"'$address'"/' /etc/yanic/yanic_$bat.conf
